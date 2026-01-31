c2369622b383dadaf08a4a20f58b8c03f20fae91
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-01-31 11:31:23 +0300

    impliment data provider

---
+++ calssroom-frontend/src/constants/index.ts
@@ -0,0 +1,22 @@
+export const DEPARTMENTS = [
+  
+  "Mathematics",
+  "Science",
+  "History",
+  "Literature",
+  "Art",
+  "Physical Education",
+  "Computer Science",
+  "Biology",
+  "Chemistry",
+  "Physics",
+  "Geography",
+  "Economics",
+  "Music",
+  "Foreign Languages",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+  label: dept,
+  value: dept,
+}));
