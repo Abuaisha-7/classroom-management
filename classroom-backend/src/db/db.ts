b1c45b27a25164d1bd148fa298dde829fd4e3876
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-01-31 19:33:17 +0300

    implement coderabbit suggested fixes

--- classroom-backend/src/db/db.ts
+++ classroom-backend/src/db/db.ts
@@ -1,10 +1,11 @@
 import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in environment");
 }
 
 const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle({ client: sql, schema });
