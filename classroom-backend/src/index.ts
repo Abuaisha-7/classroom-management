5464535519cabab15f782d8ceb57b503a7ab6f60
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-02-04 12:22:32 +0300

    Render config listen on host

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -38,6 +38,6 @@ app.get("/", (req, res) => {
   res.send("Hello, welcome to the Classroom API!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
