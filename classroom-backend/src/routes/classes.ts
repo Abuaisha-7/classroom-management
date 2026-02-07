b4776c5c3019c83f10d9392c9e165b17f028853e
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-02-07 15:49:08 +0300

    implement class detail page

--- classroom-backend/src/routes/classes.ts
+++ classroom-backend/src/routes/classes.ts
@@ -1,7 +1,7 @@
 import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
 import express from "express";
 import { db } from "../db/db.js";
-import { classes, subjects, user } from "../db/schema/index.js";
+import { classes, departments, subjects, user } from "../db/schema/index.js";
 
 const route = express.Router();
 
@@ -112,6 +112,36 @@ route.get("/", async (req, res) => {
     console.error("GET / classes error:", e);
     res.status(500).json({ error: "Failed to get classes." });
   }
+});
+
+route.get("/:id", async (req, res) => {
+  const classId = Number(req.params.id);
+
+  if (!Number.isFinite(classId))
+    return res.status(400).json({ error: "Invalid class ID." });
+
+  const [classDetails] = await db
+    .select({
+      ...getTableColumns(classes),
+      subject: {
+        ...getTableColumns(subjects),
+      },
+      department: {
+        ...getTableColumns(departments),
+      },
+      teacher: {
+        ...getTableColumns(user),
+      },
+    })
+    .from(classes)
+    .leftJoin(subjects, eq(classes.subjectId, subjects.id))
+    .leftJoin(user, eq(classes.teacherId, user.id))
+    .leftJoin(departments, eq(subjects.departmentId, departments.id))
+    .where(eq(classes.id, classId))
+
+  if (!classDetails) return res.status(404).json({ error: "No class found" });
+
+  res.status(200).json({ data: classDetails });
 });
 
 export default route;
